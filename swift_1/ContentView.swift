28bd5f89f38d57248097be4436cc00615615ff25
rbt200 <myesoap@mail.ru>
2020-03-22 03:37:39 -0700

    Initial commit

---
+++ swift_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_1
+//
+//  Created by Ivan Ivanov on 22/03/2020.
+//  Copyright © 2020 Ivan Ivanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
